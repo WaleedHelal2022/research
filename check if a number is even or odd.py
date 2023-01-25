d0b85025799a62f2e28bc6bc4a79ee5f5a7f2936
WaleedHelal2022 <119079356+WaleedHelal2022@users.noreply.github.com>
2023-01-25 21:03:12 +1300

    check if a number is even or odd
    # This is a Python script that uses an if statement to check if a number is even or odd

---
+++ check if a number is even or odd.py
@@ -0,0 +1,7 @@
+# This is a Python script that uses a if statement to check if a number is even or odd
+
+num = 3
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
