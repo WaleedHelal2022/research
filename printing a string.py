bab1cec0e116d376b0f63753e6cf007d32307413
WaleedHelal2022 <119079356+WaleedHelal2022@users.noreply.github.com>
2023-01-25 19:22:32 +1300

    printing a string
    #  example of printing a string"Hello, Name!" to the screen

---
+++ printing a string.py
@@ -0,0 +1,13 @@
+#  example of printing a string"Hello, Name!" to the screen
+
+# Define a variable
+name = "Waleed ELsayed"
+
+# Define a function
+def say_hello(name):
+    return "Hello, " + name
+
+# Call the function
+print(say_hello(name))
+
+# Output: Hello, Waleed ELsayed
